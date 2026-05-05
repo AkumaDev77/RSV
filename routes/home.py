4f276a250c0c0d0c78866ce2b8a69f52be208664
Gustavo Bryan <Gustavo.nogueira77@outlook.com>
2026-05-05 00:50:19 -0300

    remove yfinance

--- routes/home.py
+++ routes/home.py
@@ -3,7 +3,6 @@ from supabase import create_client
 from database.supabase_client import supabase
 from datetime import datetime
 import locale
-import yfinance as yf
 import uuid
 
 home_route = Blueprint('home', __name__)
